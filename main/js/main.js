e8d874de1d617d5dd3ca496cef0ce8631160e51f
stephencisler <stephencisler@gmail.com>
2017-12-29 00:26:45 -0600

    started javascript

---
+++ main/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    // jQuery selectors for keys and screen
+
+    var syb1 = $('#syb-1');
+    var syb2 = $('#syb-2');
+    var syb3 = $('#syb-3');
+    var syb4 = $('#syb-4');
+    var syb5 = $('#syb-5');
+    var syb6 = $('#syb-6');
+    var syb7 = $('#syb-7');
+    var syb8 = $('#syb-8');
+    var syb9 = $('#syb-9');
+    var syb0 = $('#syb-0');
+
+    var sybAdd = $('#syb-add');
+    var sybSub = $('#syb-sub');
+    var sybMult = $('#syb-mult');
+    var sybDiv = $('#syb-div');
+
+    var sybSq = $('#syb-sq');
+    var sybSr = $('#syb-sr');
+
+    var sybDec = $('#syb-dec');
+    var sybNeg = $('#syb-neg');
+
+    var sybDel = $('#syb-del');
+    var sybCe = $('#syb-ce');
+    var sybC = $('#syb-c');
+
+    var sybEql = $('#syb-eql');
+
+    var screen = $('#screen');
+
+    var equation = [];
+
+
+});
